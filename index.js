9998c880e2ef1ab50a77333b63c479473618f53f
Dani BL <danibachsl@protonmail.com>
2022-06-21 15:57:41 +0200

    max y min a punto de funcionar

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    this.items.sort(function(a, b) {
+      return a - b;
+    });
+    return this.items;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos] !== undefined) {
+      return this.items[pos];
+    } else {
+      throw new Error('OutOfBounds');
+    }
+  }
 
-  max() {}
+  max() {
+    if (this.items.length !== 0) {
+      return Mathmax(...this.items);
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.items.length !== 0) {
+      return Mathmin(...this.items);
+    } else {
+      throw new Error('EmptySortedList');
+    }
+  }
 
   sum() {}
 
